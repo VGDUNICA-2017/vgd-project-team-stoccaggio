2f70ccf5fc87a16f7e46756911029f9cf5ab6d57
dcardia3 <francesco.cardia1954@tiscali.it>
2018-04-17 10:50:37 +0200

    Sistemazione cartelle + fix grafica

---
+++ Il Viaggio/Assets/Scripts/Game/FadeAudioClip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAudioClip : MonoBehaviour {
+
+    public float fadeSpeed = 0.005f;
+
+    public float volumeLimit = 1.0f;
+
+    public FadeDirection fadeDirection = FadeDirection.None;
+
+    public enum FadeDirection
+    {
+        None,
+        Louder,
+        Quieter
+    }
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate () {
+		
+        if(fadeDirection != FadeDirection.None)
+        {
+            if(fadeDirection == FadeDirection.Louder && audioSource.volume != volumeLimit)
+                audioSource.volume += fadeSpeed;
+            else if(fadeDirection == FadeDirection.Quieter && audioSource.volume != volumeLimit)
+                audioSource.volume -= fadeSpeed;
+        }
+    }
+}
